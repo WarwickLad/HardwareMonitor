510e7b6c6fab67e9dd67815ae1a67e25ffacdaa1
WarwickLad <zacharylaker123@gmail.com>
2020-04-26 21:28:59 +0100

    Initial commit

---
+++ HardwareMonitor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+public class HardwareMonitor
+{
+    public PerformanceCounter cpuCounter;
+    public PerformanceCounter ramCounter;
+
+    public void setCurrentCpuUsage()
+    {
+        this.cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+    }
+    public void setramCounter()
+    {
+        this.ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+    }
+
+    public string getCurrentCpuUsage()
+    {
+        return cpuCounter.NextValue() + "%";
+    }
+
+    public string getAvailableRAM()
+    {
+        return ramCounter.NextValue() + "MB";
+    }
+
+    private static void RunTest()
+    {
+        HardwareMonitor hardwareMonitorInstance = new HardwareMonitor();
+        String consoleOutput;
+        while (true)
+        {
+            hardwareMonitorInstance.setramCounter();
+            consoleOutput = hardwareMonitorInstance.getAvailableRAM();
+            Console.WriteLine(consoleOutput);
+            Console.ReadLine();
+        }
+        
+    }
+
+    static void Main()
+    {
+        RunTest();
+    }
+}
